137b5ac99df960648aaf844013feeba46349161d
lidarmansiwon <kimsanmaro@naver.com>
2025-05-26 00:59:11 +0900

    study sub

--- src/helloworld_subscriber.cpp
+++ src/helloworld_subscriber.cpp
@@ -12,6 +12,8 @@ public:
   HelloworldSubscriber()
   : Node("Helloworld_subscriber")
   {
+    // 여기서 KeepLast(10)은 Qos에서 depth를 10으로 하는 것. 통신이 원할하지 않을 경우,
+    // 받은 데이터를 버퍼에 10개까지 저장하는 것. 
     auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
     helloworld_subscriber_ = this->create_subscription<std_msgs::msg::String>(
         "helloworld",
@@ -20,6 +22,18 @@ public:
   }
 
 private:
-  
+  void subscribe_topic_message(const std_msgs::msg::String::SharedPtr msg) const
+  {
+    RCLCPP_INFO(this->get_logger(), "Received Message: '%s'", msg->data.c_str());
+  }
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr helloworld_subscriber_;
+};
 
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<HelloworldSubscriber>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
